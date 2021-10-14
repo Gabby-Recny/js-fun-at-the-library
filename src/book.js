9a698b81642341ab4cc906c20b760e27d32da5dc
Gabby Recny <gjtrecny@gmail.com>
2021-10-13 21:56:44 -0500

    Complete half test

--- src/book.js
+++ src/book.js
@@ -1,11 +1,28 @@
-function createTitle() {
-  
+function createTitle(bookIdea) {
+  return `The ${bookIdea}`
 }
+
+
+function buildMainCharacter (name, age, pronouns) {
+  var mainCharacter = {
+    name: name,
+    age: age,
+    pronouns: pronouns,
+  }
+  return mainCharacter;
+}
+
+function saveReview(bookReview, reviews) {
+  reviews.push(bookReview)
+  reviews.length;
+}
+
+
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
+  buildMainCharacter,
+  saveReview,
   // calculatePageCount,
   // writeBook,
   // editBook
