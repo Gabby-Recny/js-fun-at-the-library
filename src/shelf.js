e9b9127a9f7ecbe7df5ac9119c7baadef113bcc5
Gabby Recny <gjtrecny@gmail.com>
2021-10-14 20:33:51 -0500

    Complete first three tests

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,35 @@
+function shelfBook(bookTitle, genreShelf) {
+  if (genreShelf.length < 3)  {
+    genreShelf.unshift(bookTitle);
+  }
+}
+
+function unshelfBook(bookTitle, genreShelf) {
+  for(var i = 0; i < genreShelf.length; i++) {
+    if (genreShelf[i].title === bookTitle) {
+      genreShelf.splice(i, 1)
+    }
+  }
+}
+
+function listTitles(genreShelf) {
+  var titles = [];
+  for(var i = 0; i < genreShelf.length; i++) {
+    titles.push(genreShelf[i].title)
+  }
+  var newTitles = titles.join(', ');
+  return newTitles;
+}
+
+function searchShelf() {
+
+}
+
+
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
